8320dd17a2ec583966e13a5d48b0ee73ed71598a
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2023-05-10 23:24:56 -0300

    feat: Impedindo acesso a rota de confirmação quando não houver dados válidos

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { DefaultLayout } from './layouts/DefaultLayout';
+import { ProductsContext } from './contexts/products/ProductsContext';
 
 import { Home } from './pages/Home';
 import { Order } from './pages/Order';
 import { Confirmation } from './pages/Confirmation';
 
 export function Router() {
+  const { productsSelected, address, payment } = useContext(ProductsContext);
+
+  function elementPathConfirmationByAccess(): JSX.Element {
+    const allDataValid = !!(productsSelected.length && address && payment);
+
+    return allDataValid ? <Confirmation /> : <Navigate to="/" />;
+  }
+
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/order" element={<Order />} />
-        <Route path="/confirmation" element={<Confirmation />} />
+        <Route
+          path="/confirmation"
+          element={elementPathConfirmationByAccess()}
+        />
       </Route>
     </Routes>
   );
